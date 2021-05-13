0c9dfc49c0240ff8cca2784b140e57cca965c05d
Lombardo <jeffsec@amazon.com>
2021-05-13 15:35:36 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
-## My Project
+# Purpose
+This Lambda function will clean a DynamoDB table used by the [Device Grant flow logic for Cognito](https://github.com/aws-samples/cognito-device-grant-flow) by:
+ - Expiring non expired Codes that reach their maximum lifetime
+ - Deleting non expired and expired Codes that reach their maximum visibility lifetime
 
-TODO: Fill this README out!
+# How to deploy it?
+ - Create a new Lambda function (NodeJS based, tested with nodejs10.x)
+ - Associate a IAM Execurtion role that allows:
+   - Basic Lambda Execution permissions
+   - Access to the DynamoDB table for Read, Update, and Delete operations
+ - Create two environment variable:
+   - DYNAMODB_TABLE that references the DynamoDB table that store the Codes and their status
+   - DYNAMODB_MAX_VISIBILITY that defines the maximum visibility lifetime in seconds after expiration
+ - Create a CloudWatch scheduled event following instructions at: https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/RunLambdaSchedule.html
 
-Be sure to:
-
-* Change the title in this README
-* Edit your repository description on GitHub
-
-## Security
+# Security
 
 See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
 
-## License
+# License
 
 This library is licensed under the MIT-0 License. See the LICENSE file.
 
---
+++ index.js
@@ -0,0 +1,161 @@
+'use strict';
+
+var AWS = require("aws-sdk");
+
+exports.handler = (event, context, callback) => {
+    console.log('LogScheduledEvent');
+    //Receive CloudWatch scheduled event
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    
+    //Bootstrap DynamoDB Client
+    var dynamodb = new AWS.DynamoDB();
+    
+    //Search for all entries that are not yet expired cause entries are automatically expired if necessary when polled
+    var params = {
+        ExpressionAttributeNames: {
+            "#Status": "Status"
+        }, 
+        ExpressionAttributeValues: {
+            ":expired": {
+                S: "expired"
+            }
+        }, 
+        FilterExpression: "#Status <> :expired", 
+        TableName: process.env.DYNAMODB_TABLE
+    };
+    dynamodb.scan(params, function(err, data) {
+        if (err) {
+            //There was an error when collecting the data
+            console.log("Error while scanning DB for 'non-expired");
+            console.log(err, err.stack); // an error occurred
+        } else {
+            console.log("Processing result set");
+            //If the Result Set is not empty  
+            if (data.Items.length > 0) {
+                var item;
+                //For any Result in the Result set
+                for ( item in data.Items) {
+                    //If the Result Lifetime is older than the DYNAMODB_MAX_VISIBILITY
+                    if ( Date.now() > (new Date(parseInt(data.Items[item].Max_expiry.S)) + process.env.DYNAMODB_MAX_VISIBILITY) ) {
+                        //Delete the codes from the DB
+                        params = {
+                            Key: {
+                                "Device_code": {
+                                    S: data.Items[item].Device_code.S
+                                }
+                            },
+                            TableName: process.env.DYNAMODB_TABLE,
+                        };
+                        dynamodb.deleteItem(params, function(err, data) {
+                           if (err) {
+                               //An error occured during the deletion
+                               console.log("Error occured when deleting item from the DB");
+                               console.log(err, err.stack); // an error occurred
+                           } else {
+                               //Result is deleted
+                               console.log("Item deleted from DB");
+                           }
+                         });
+                  //If the Result Lifetime is older maximum lifetime
+                  } else if ( Date.now() > (new Date(parseInt(data.Items[item].Max_expiry.S))) ) {
+                      //Expire the codes in the DB
+                      params = {
+                            ExpressionAttributeNames: {
+                                "#Status": "Status"
+                            },
+                            ExpressionAttributeValues: {
+                                ":status": {
+                                    S: "expired"
+                                }
+                            }, 
+                            Key: {
+                                "Device_code": {
+                                    S: data.Items[item].Device_code.S
+                                }
+                            },
+                            ReturnValues: "ALL_NEW", 
+                            TableName: process.env.DYNAMODB_TABLE,
+                            UpdateExpression: "SET #Status = :status"
+                        };
+                        dynamodb.updateItem(params, function(err, data) {
+                            if (err) {
+                                //An error happened while updating teh status of the codes
+                                console.log("Error occured when updating the DB to expired");
+                                console.log(err, err.stack);
+                            } else {
+                                //Codes have been expired
+                                console.log("DB has been updated");
+                            }
+                        });
+                  } else  {
+                      //Codes are still valid
+                      console.log("Codes are still valid");
+                  }
+              }
+          } else {
+              //There were no result set
+              console.log("No non-expired result set");
+          }
+      }
+    });
+    //Search for all entries that are not expired
+    params = {
+      ExpressionAttributeNames: {
+       "#Status": "Status"
+      }, 
+      ExpressionAttributeValues: {
+       ":expired": {
+         S: "expired"
+        }
+      }, 
+      FilterExpression: "#Status = :expired", 
+      TableName: "DeviceGrant"
+     };
+    dynamodb.scan(params, function(err, data) {
+      if (err) {
+          //There was an error when collecting the data
+          console.log("Error while scanning DB for 'expired'");
+          console.log(err, err.stack); // an error occurred
+      } else {
+          console.log("Processing result set");  
+          //If the Result Set is not empty  
+          if(data.Items.length > 0) {
+              var item;
+              for ( item in data.Items) {
+                  //If the Result Lifetime is older than the DYNAMODB_MAX_VISIBILITY
+                  var limit = new Date(parseInt(data.Items[item].Max_expiry.S));
+                  limit = limit + process.env.DYNAMODB_MAX_VISIBILITY;
+                  if ( Date.now() > limit ) {
+                      //Delete the codes from the DB
+                      params = {
+                          Key: {
+                              "Device_code": {
+                                  S: data.Items[item].Device_code.S
+                                }
+                            },
+                            TableName: process.env.DYNAMODB_TABLE,
+                        };
+                        dynamodb.deleteItem(params, function(err, data) {
+                            if (err) {
+                                //An error occured during the deletion
+                                console.log("Error occured when deleting item from the DB");
+                                console.log(err, err.stack); // an error occurred
+                            } else {
+                                //Result is deleted
+                                console.log("Item deleted from DB");
+                            }
+                        });
+                    } else {
+                        //Codes are still valid
+                        console.log("Codes are still valid");
+                    }
+                }
+            } else {
+                //There were no result set
+                console.log("No expired result set");
+            }
+        }
+    });
+                
+    callback(null, 'Finished');
+};
